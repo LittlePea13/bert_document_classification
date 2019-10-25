08700130686e2a0979741e2d39281cc048f19a5f
Pere Lluis Huguet Cabot <phuguetcabot@amsterdamdatacollective.com>
2019-10-25 16:46:16 +0200

    fix where

--- bert_document_classification/document_bert.py
+++ bert_document_classification/document_bert.py
@@ -175,7 +175,7 @@ class BertForDocumentClassification():
         document_representations, document_sequence_lengths  = encode_documents(train_documents, self.bert_tokenizer)
 
         correct_output = torch.FloatTensor(train_labels)
-        binary_output = torch.where(correct_output > 0) 
+        binary_output = torch.where(correct_output > 0, torch.ones(correct_output.shape),torch.zeros(correct_output.shape)) 
         loss_weight = ((binary_output.shape[0] / torch.sum(binary_output, dim=0))-self.args['loss_bias']).to(device=self.args['device'])
         self.loss_function = torch.nn.BCEWithLogitsLoss(reduction='none',pos_weight=loss_weight)
         self.log.info('Loss weight: %f' % (loss_weight))
@@ -205,9 +205,9 @@ class BertForDocumentClassification():
                                                                  freeze_bert=self.args['freeze_bert'], device=self.args['device'])
 
                 batch_correct_output = correct_output[i:i + self.args['batch_size']].to(device=self.args['device'])
-                batch_binary_output = torch.where(batch_correct_output > 0) 
+                batch_binary_output = torch.where(batch_correct_output > 0, torch.ones(batch_correct_output.shape), torch.zeros(batch_correct_output.shape)) 
                 loss = self.loss_function(batch_predictions, batch_binary_output)
-                loss = loss * torch.where(batch_correct_output == 2 or batch_correct_output == -1, 2, 1)
+                loss = loss * torch.where(batch_correct_output == 2 or batch_correct_output == -1, 2*torch.ones(batch_correct_output.shape), orch.ones(batch_correct_output.shape))
                 loss = loss.mean()
                 epoch_loss += float(loss.item())
                 #self.log.info(batch_predictions)
